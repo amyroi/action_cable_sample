b7664e96bc44d50e940bc253174b4f32a53b532c
Atsuko Mori <atsuko.mori.200@gmail.com>
2018-08-01 18:10:12 +0900

    クライアントからサーバーに通信
    - Web consoleから App.activity.speak(hello)

--- app/channels/activity_channel.rb
+++ app/channels/activity_channel.rb
@@ -1,12 +1,13 @@
 class ActivityChannel < ApplicationCable::Channel
   def subscribed
-    stream_from 'activiry'
+    stream_from 'activity'
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def speak
+  def speak(data)
+    ActionCable.server.broadcast 'activity', message: data['message']
   end
 end
